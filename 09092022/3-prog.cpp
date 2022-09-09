2dd9e62b873941f46d4e3c86040e5fd1bcfc7616
kroninberg <utypostev@gmail.com>
2022-09-09 12:06:35 +0300

    09092022 1-st commit

---
+++ 09092022/3-prog.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+    //setlocale(LC_ALL, "");
+    std::cout << "Первая строка\nВторая строка\n";
+    return 0;
+}
---
+++ 09092022/4-prog.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std::cout << "First string\n";
+    return 0;
+}
